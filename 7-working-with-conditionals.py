389302eead3f83e210edc2a38ada50505b833c9d
SendyPrismanaNurferian <sendyprisma02@gmail.com>
2025-11-17 13:26:28 +0000

    upload tugas terbaru (7-working-with-conditionals.py)

---
+++ 7-working-with-conditionals.py
@@ -0,0 +1,23 @@
+# Conditionals dalam Python 
+# Latihan 1 & 2: Mengirim Paket (if dan else)
+userReply = input("Do you need to ship a package? (Enter yes or no) ")
+
+if userReply == "yes":
+    print("We can help you ship that package!")
+else:
+    print("Please come back when you need to ship a package. Thank you.")
+    exit() # <--- Perintah ini akan mematikan program seketika.
+
+# Latihan 3: Layanan Tambahan (elif)
+# Kita menggunakan variabel yang sama (userReply) untuk input baru
+userReply = input("Would you like to buy stamps, buy an envelope, or make a copy? (Enter stamps, envelope, or copy) ")
+
+if userReply == "stamps":
+    print("We have many stamp designs to choose from.")
+elif userReply == "envelope":
+    print("We have many envelope sizes to choose from.")
+elif userReply == "copy":
+    copies = input("How many copies would you like? (Enter a number) ")
+    print("Here are {} copies.".format(copies))
+else:
+    print("Thank you, please come again.")
